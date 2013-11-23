36e104e720dd17440793f7700938e23e9b4f93ad
pcebulsk <cebulski69@gmail.com>
2013-11-23 15:51:11 +0100

    Napisalem te funkcje tak jak w tych materialach sa bo w tej ksiazce to inaczej bylo jednak. Na przykladzie 3x3 dziala.

--- main.c
+++ main.c
@@ -3,72 +3,120 @@
 #include <stdio.h>
 #include "nrutil.h"
 
-void choldc(float **a, int n, float p[])
+/*
+Skopiowane z ksiazki Numerica Recepies
+*/
+//void choldc(float **a, int n, float p[])
+//{
+//    void nrerror(char error_text[]);
+//    int i,j,k;
+//    float sum;
+//
+//    for (i=1;i<=n;i++)
+//    {
+//        for (j=i;j<=n;j++)
+//        {
+//            for (sum=a[i][j],k=i-1;k>=1;k--) sum -= a[i][k]*a[j][k];
+//            if (i == j)
+//            {
+//                if (sum <= 0.0) nrerror("choldc failed");
+//                p[i]=sqrt(sum);
+//            } else a[j][i]=sum/p[i];
+//        }
+//    }
+//}
+//void cholsl(float **a, int n, float p[], float b[], float x[])
+//{
+//    int i,k;
+//    float sum;
+//
+//    for (i=1;i<=n;i++)
+//    {
+//        for (sum=b[i],k=i-1;k>=1;k--) sum -= a[i][k]*x[k];
+//        x[i]=sum/p[i];
+//    }
+//    for (i=n;i>=1;i--)
+//    {
+//        for (sum=x[i],k=i+1;k<=n;k++) sum -= a[k][i]*x[k];
+//        x[i]=sum/p[i];
+//    }
+//}
+
+/*
+Wersja pierwsza ze slajdów
+*/
+void choldc(float **a, float **l, int n)
 {
     void nrerror(char error_text[]);
     int i,j,k;
     float sum;
 
-    for (i=1;i<=n;i++)
+    for (k = 1; k <= n; k++)
     {
-        for (j=i;j<=n;j++)
+        for (sum = a[k][k], j = 1; j <= k - 1; j++) sum -= l[k][j] * l[k][j];
+        l[k][k] = sqrt(sum);
+        for (i = k + 1; i <= n; i++)
         {
-            for (sum=a[i][j],k=i-1;k>=1;k--) sum -= a[i][k]*a[j][k];
-            if (i == j)
+            for (sum = a[i][k], j = 1; j <= k - 1; j++) sum -= l[i][j] * l[k][j];
+            if (k == i)
             {
                 if (sum <= 0.0) nrerror("choldc failed");
-                p[i]=sqrt(sum);
-            } else a[j][i]=sum/p[i];
+            } else l[i][k] = sum / l[k][k];
         }
     }
 }
 
-void cholsl(float **a, int n, float p[], float b[], float x[])
+/*
+Wersja druga ze slajdów
+*/
+void choldc2(float **a, float **l, int n)
 {
-    int i,k;
+    void nrerror(char error_text[]);
+    int i,j,k;
     float sum;
 
-    for (i=1;i<=n;i++)
+    for (k = 1; k <= n - 1; k++)
     {
-        for (sum=b[i],k=i-1;k>=1;k--) sum -= a[i][k]*x[k];
-        x[i]=sum/p[i];
+        l[k][k] = sqrt(a[k][k]);
+        for (i = k + 1; i <= n; i++) l[i][k] = a[i][k] / l[k][k];
+        for (j = k + 1; j <= n; j++)
+        {
+            for (i = j; i <= n; i++) a[i][j] = a[i][j] - l[i][k] * l[j][k];
+        }
     }
-    for (i=n;i>=1;i--)
-    {
-        for (sum=x[i],k=i+1;k<=n;k++) sum -= a[k][i]*x[k];
-        x[i]=sum/p[i];
-    }
+    l[n][n] = sqrt(a[n][n]);
 }
+
+
 
 
 int main(void)
 {
     int n = 3;
-    float *x;
-    x = vector(1, 3);
-    float *p;
-    p = vector(1, 3);
-    float *b;
-    b = vector(1, 3);
-    b[1] = 2;
-    b[2] = 8;
-    b[3] = 10;
+    float **l;
+    l = matrix(1, 3, 1, 3);
     float **a;
     a = matrix(1, 3, 1, 3);
-    a[1][1] = 1;
-    a[2][1] = 2;
-    a[3][1] = 3;
-    a[1][2] = 2;
-    a[2][2] = 8;
-    a[3][2] = 10;
-    a[1][3] = 3;
-    a[2][3] = 10;
-    a[3][3] = 22;
+    //przyklad z angielskiej wiki http://en.wikipedia.org/wiki/Cholesky_decomposition#Example
+    a[1][1] = 4;
+    a[2][1] = 12;
+    a[3][1] = -16;
+    a[1][2] = 12;
+    a[2][2] = 37;
+    a[3][2] = -43;
+    a[1][3] = -16;
+    a[2][3] = -43;
+    a[3][3] = 98;
 
-    choldc(a, n, p);
-    cholsl(a, n, p, b, x);
+    choldc(a, l, n);
+    printf("%f\t0\t\t0\n", l[1][1], l[1][2], l[1][3]);
+    printf("%f\t%f\t0\n", l[2][1], l[2][2], l[2][3]);
+    printf("%f\t%f\t%f\n", l[3][1], l[3][2], l[3][3]);
 
-    printf("%f %f %f\n", x[1], x[2], x[3]);
+    choldc2(a, l, n);
+    printf("\n\n%f\t0\t\t0\n", l[1][1], l[1][2], l[1][3]);
+    printf("%f\t%f\t0\n", l[2][1], l[2][2], l[2][3]);
+    printf("%f\t%f\t%f\n", l[3][1], l[3][2], l[3][3]);
 
-    return 0;
+return 0;
 }
